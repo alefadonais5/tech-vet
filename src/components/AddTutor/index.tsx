9306985a8ff41bb83b2c47c9aba9c958ec85f3c1
Alef Adonais <alefadonais5@gmail.com>
2024-12-03 20:16:56 -0300

    feat: criar componente cadastrar tutor

--- src/components/AddTutor/index.tsx
+++ src/components/AddTutor/index.tsx
@@ -2,7 +2,55 @@ import React, { useEffect, useState } from "react";
 
 
 export default function AddTutor(){
+return(
+    <div>
+        <h1>Cadastrar Novo Tutor</h1>
 
+        <form action="">
+            <label htmlFor="tutor-name">Nome:</label>
+            <input type="text" id="tutor-name" name="tutorName"/>
+
+            <label htmlFor="cpf-tutor">CPF:</label>
+            <input type="number" id="cpf-tutor" name="cpf-tutor"/>
+
+            <label htmlFor="rg-tutor">RG:</label>
+            <input type="text" id="rg-tutor" name="rg-tutor"/>
+
+            <label htmlFor="date-tutor">Data de Nascimento:</label>
+            <input type="date" id="date-tutor" name="date-tutor"/>
+
+            <label htmlFor="sexo-tutor">Sexo:</label>
+            <input type="text" id="sexo-tutor" name="sexo-tutor"/>
+
+            <h2>Dados para contato:</h2>
+
+            <label htmlFor="cep-tutor">CPF:</label>
+            <input type="number" id="cep-tutor" name="cep-tutor"/>
+
+            <label htmlFor="address-tutor">Endereço:</label>
+            <input type="text" id="address-tutor" name="address-tutor"/>
+
+            <label htmlFor="number-tutor">Número:</label>
+            <input type="number" id="number-tutor" name="number-tutor"/>
+
+            <label htmlFor="bairro-tutor">Bairro:</label>
+            <input type="text" id="bairro-tutor" name="bairro-tutor"/>
+            
+            <label htmlFor="city-tutor">Cidade:</label>
+            <input type="text" id="city-tutor" name="city-tutor"/>
+
+            <label htmlFor="state-tutor">Estado:</label>
+            <input type="text" id="state-tutor" name="state-tutor"/>
+            
+            <label htmlFor="tel-tutor">Telefone:</label>
+            <input type="tel" id="tel-tutor" name="tel-tutor"/>
+
+            <label htmlFor="email-tutor">Email:</label>
+            <input type="text" id="email-tutor" name="email-tutor"/>
+            
+            <button type="submit"> Cadastrar tutor</button>
+        </form>
+    </div>
+)
     
-
 }
--- src/pages/tutor/index.tsx
+++ src/pages/tutor/index.tsx
@@ -1,12 +1,15 @@
+import AddTutor from "@/components/AddTutor";
 import { Header } from "@/components/Header";
 import React, { useEffect, useState } from "react";
 
 
 export default function TutorVet(){
-
     return(
-        <Header />
-    )
-
+        <>
+        {/* <Header /> */}
+        <AddTutor />
+        
+        </>
+    );
 }
 
