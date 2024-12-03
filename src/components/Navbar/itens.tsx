ebe5682b1fae7ee6d18e471ce10c146b3555f7d3
Alef Adonais <alefadonais5@gmail.com>
2024-12-03 16:56:34 -0300

    feat: cria novos itens na navbar

--- src/components/Navbar/itens.tsx
+++ src/components/Navbar/itens.tsx
@@ -1,5 +1,6 @@
 import ForecastIcon from "@/ui/assets/icons/Forecast";
 import HospitalIcon from "@/ui/assets/icons/Hospital";
+import IconPaw from "@/ui/assets/icons/IconPaw";
 import LogoutIcon from "@/ui/assets/icons/Logout ";
 import PersonIcon from "@/ui/assets/icons/Person";
 import ServiceIcon from "@/ui/assets/icons/Service";
@@ -22,18 +23,24 @@ export const itens = [
   },
   {
     id: "3",
+    icon: <IconPaw />,
+    title: "Animais",
+    path: "/animal",
+  },
+  {
+    id: "4",
     icon: <HospitalIcon />,
     title: "Internamentos",
     path: "/consultas",
   },
   {
-    id: "4",
+    id: "5",
     icon: <StockIcon />,
     title: "Estoque",
     path: "/consultas",
   },
   {
-    id: "5",
+    id: "6",
     icon: <ForecastIcon />,
     title: "Previsão de estoque",
     path: "/consultas",
